b56884e6acd97c603b7cbcd8a741101d4c98175d
Vilas Shetkar <vilasshetkar@gmail.com>
2024-08-13 18:14:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ console.log('This is server file');
 process.env.SECRET_KEY = 'secret-key-ABC123';
 
 // Connect to MongoDB using Mongoose new change
-mongoose.connect('mongodb+srv://vhshetkar:tkScZ7GTDHgQ8Kr2@cluster0.umtsvuw.mongodb.net/Org', {
+mongoose.connect('mongodb+srv://ApnaERP-Admin:YJtGrIp4NZ6MRKdF@cluster1.2lk9mic.mongodb.net/USER-SERVICE', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
